11dd2aaac73ea507a5044d8f870b2d6c7b0690c2
menelike <mitschnacker@gmx.net>
2018-02-24 17:57:09 +0100

    be able to send visually silenced (no tray updates, no pop ups) content updated messages { contentUpdate: "0" | "1" }

--- src/android/com/adobe/phonegap/push/FCMService.java
+++ src/android/com/adobe/phonegap/push/FCMService.java
@@ -98,6 +98,7 @@ public class FCMService extends FirebaseMessagingService implements PushConstant
       String titleKey = prefs.getString(TITLE_KEY, TITLE);
 
       extras = normalizeExtras(applicationContext, extras, messageKey, titleKey);
+      int contentUpdate = parseInt(CONTENT_UPDATE, extras);
       String clearNotifications = extras.getString(CLEAR_NOTIFICATIONS);
 
       if (clearBadge) {
@@ -120,8 +121,18 @@ public class FCMService extends FirebaseMessagingService implements PushConstant
         }
       }
 
+      if (contentUpdate == 1) {
+        int notId = parseInt(NOT_ID, extras);
+
+        if (messageMap.containsKey(notId)) {
+          extras.putBoolean(FOREGROUND, false);
+          extras.putBoolean(COLDSTART, PushPlugin.isActive());
+
+          showNotificationIfPossible(applicationContext, extras);
+        }
+      }
       // if we are in the foreground and forceShow is `false` only send data
-      if (!forceShow && PushPlugin.isInForeground()) {
+      else if (!forceShow && PushPlugin.isInForeground()) {
         Log.d(LOG_TAG, "foreground");
         extras.putBoolean(FOREGROUND, true);
         extras.putBoolean(COLDSTART, false);
